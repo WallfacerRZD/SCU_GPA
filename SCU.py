0348b0175e39216e66665c50a20ef12ade67d50a
WallfacerRZD <1048058400@qq.com>
2018-01-21 21:31:56 +0800

    py3

--- SCU.py
+++ SCU.py
@@ -1,99 +1,48 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
+# coding:utf-8
 from course_parser import Parser
-import cookielib
-import urllib2
-import urllib
-import sys
+import requests
 
 
 class SCU(object):
     def __init__(self):
         self.url = 'http://zhjw.scu.edu.cn/login.jsp'
-        self.login_by_post_url = 'http://zhjw.scu.edu.cn/loginAction.do'
+        self.login_url = 'http://zhjw.scu.edu.cn/loginAction.do'
         self.grade_url = 'http://zhjw.scu.edu.cn/gradeLnAllAction.do?type=ln&oper=fainfo&fajhh=3136'
-        print u'请输入教务处账号:'
-        self.account = raw_input()
-        print u'请输入教务处密码:'
-        self.password = raw_input()
-        print u'正在登录...'
+        self.account = input('请输入教务处账号:')
+        self.password = input('请输入教务处密码:')
+        print('正在登录...')
         self.has_logined = False
-
-    #使用selenium获取成绩页面
-    def login_by_selenium(self):
-        try:
-            self.browser = webdriver.Chrome()
-            self.browser.set_window_position(-2000, 0)
-            self.browser.get(self.url)
-            account_elem = self.browser.find_element_by_name("zjh")
-            password_elem = self.browser.find_element_by_name("mm")
-            account_elem.send_keys(self.account)
-            password_elem.send_keys(self.password)
-            self.browser.find_element_by_id("btnSure").click()
-            print u'登录成功!'
-            return
-        except:
-            pass
-
-    def get_score_page(self):
-        try:
-            self.browser.get(self.grade_url)
-            page = self.browser.page_source
-            print type(page)
-            self.browser.quit()
-            return page
-        except:
-            print u'获取成绩页面失败'
-            return None
 
     # 使用cookie登录,获取成绩页面
 
-    def get_login_cookie(self):
-        cookie = cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-        data = urllib.urlencode({
+    def login(self):
+        s = requests.Session()
+        form_data = {
             'zjh': self.account,
-            'mm': self.password
-        })
-        request = urllib2.Request(self.login_by_post_url, data)
-        response = opener.open(request)
-        if response.code == 200:
-            print u'登录成功'
-            return cookie
-        else:
-            print u'登录失败'
-            return
+            'mm': self.password,
+        }
+        s.post(self.login_url, data=form_data)
+        return s
 
-    def get_score_page_by_cookie(self):
+    def get_score_page(self):
         try:
-            cookie = self.get_login_cookie()
-            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-            request = urllib2.Request(self.grade_url)
-            response = opener.open(request)
-            page = response.read().decode('gb2312').encode('utf-8')
+            s = self.login()
+            response = s.get(self.grade_url)
+            page = response.text
             return page
         except:
-            print u'获取成绩页面失败'
+            print('获取成绩页面失败,请检查账号密码')
             return None
 
     def query_GPA(self):
-        page = self.get_score_page_by_cookie()
-        with open('test.html', 'w') as f:
-            f.write(page)
+        page = self.get_score_page()
         if page is not None:
             parser = Parser(page)
-            parser.output()
+            return parser.output()
         else:
-            print u'解析失败'
-            return
+            return '解析失败'
+
 
 if __name__ == '__main__':
     SCU_spider = SCU()
     SCU_spider.query_GPA()
-
-
-
-
-
-
-
--- course_parser.py
+++ course_parser.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
+# coding:utf-8
 from bs4 import BeautifulSoup
+
 
 # 格式化输出,解决Unicode字符站2个ascii字符宽度
 
-def format_print(my_str, width=40):
+def format_print(my_str, width=40, end=''):
     real_width = 0
     for i in my_str:
         if ord(i) > 127:
             real_width += 2
         else:
             real_width += 1
-    print my_str + ' ' * (width - real_width),
+    print(my_str + ' ' * (width - real_width), end=end)
 
 
 class Course(object):
-
     def __init__(self, name, credit, attr, score, reason):
         self.name = name
         self.credit = credit
@@ -27,21 +27,19 @@ class Course(object):
         format_print(self.credit, 10)
         format_print(self.attr, 10)
         format_print(self.score, 10)
-        format_print(self.reason, 10)
-        print
+        format_print(self.reason, 10, '\n')
 
 
 class Parser(object):
     def __init__(self, page):
         self.soup = BeautifulSoup(page, 'html.parser')
-        print u'正在解析成绩....'
+        print('正在解析成绩....')
 
     def get_title(self):
         return self.soup.find('table', border="0", align="left", cellpadding="0").find('b').string
 
     def get_table_node(self):
         return self.soup.find('table', class_="displayTag")
-
 
     # [课程号, 课序号, 课程名, 英文课程名, 学分, 课程属性, 成绩, 未通过原因]
     def get_table_head(self, table_node):
@@ -62,14 +60,11 @@ class Parser(object):
                 return child_node[1].string.strip()
 
         course_nodes = table_node.find_all('tr')
-        # 使用selenium时要用
-        # course_nodes = table_node.find('tbody').find_all('tr')
-        # 因为用浏览器打开HTML和实际的格式不一样,奇怪的bug
         del course_nodes[0]
         courses = []
         for course_node in course_nodes:
             # 解析数据
-            datas = map(parse_data, course_node.find_all('td'))
+            datas = list(map(parse_data, course_node.find_all('td')))
             course = Course(datas[2], datas[4], datas[5], datas[6], datas[7])
             courses.append(course)
         return courses
@@ -80,35 +75,39 @@ class Parser(object):
                 format_print(table_titles[i], 40)
             else:
                 format_print(table_titles[i], 10)
-        print
+        print()
 
     def get_grade_piont(self, grade):
-        if grade >= 95:
-            return 4.0
         if grade >= 90:
-            return 3.8
+            return 4.0
         if grade >= 85:
-            return 3.6
+            return 3.7
         if grade >= 80:
-            return 3.2
-        if grade >= 75:
+            return 3.3
+        if grade >= 76:
+            return 3
+        if grade >= 73:
             return 2.7
         if grade >= 70:
-            return 2.2
-        if grade >= 65:
+            return 2.3
+        if grade >= 66:
+            return 2
+        if grade >= 63:
             return 1.7
+        if grade >= 61:
+            return 1.3
         if grade >= 60:
-            return 1.0
+            return 1
         if grade < 60:
-            return 0.0
+            return 0
 
-    def get_GPA(self, credits, scores):
+    def get_GPA(self, _credits, scores):
         total_credits = 0
         # 绩点*学分的和
         product_sum = 0
-        for i in range(len(credits)):
-            total_credits += credits[i]
-            product_sum += self.get_grade_piont(scores[i]) * credits[i]
+        for i in range(len(_credits)):
+            total_credits += _credits[i]
+            product_sum += self.get_grade_piont(scores[i]) * _credits[i]
 
         return product_sum / total_credits
 
@@ -125,21 +124,20 @@ class Parser(object):
         for course in courses:
             course.show()
         # GPA,average
-        credits = map(lambda x: float(x.credit), courses)
-        scores = map(lambda x: float(x.score), courses)
-        GPA = self.get_GPA(credits, scores)
-        average = self.get_average(credits, scores)
-        print '............................................'
-        print u'GPA:', GPA
-        print u'平均分:', average
+        _credits = list(map(lambda x: float(x.credit), courses))
+        scores = list(map(lambda x: float(x.score), courses))
+        GPA = self.get_GPA(_credits, scores)
+        average = self.get_average(_credits, scores)
+        print('..................................................................................')
+        print('GPA:', GPA)
+        print('平均分:', average)
 
     def output(self):
         title = self.get_title()
         table_node = self.get_table_node()
         table_titles = self.get_table_head(table_node)
         courses = self.get_course_datas(table_node)
-        print '............................................'
-        print title
+        print('..................................................................................')
+        print(title)
         self.output_titles(table_titles)
         self.output_courses_info(courses)
-
